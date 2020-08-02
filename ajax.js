4ccb324f2ec9cf9925bcf0307cd60fc8065f2177
elina-wang <>
2020-08-02 12:48:38 +0800

    檔案連接及迴圈設定

---
+++ ajax.js
@@ -0,0 +1,24 @@
+//作用在INDEX的餐廳DIV
+
+new Vue({
+    el: "#restaurant",
+    data: {
+        datas: null // 資料: 空值
+    },
+
+    //當頁面準備好時，開始執行
+    mounted() {
+        var xhr = new XMLHttpRequest();
+        xhr.open ("GET", "https://datacenter.taichung.gov.tw/swagger/OpenData/72b2e32b-74e4-4000-b920-7457c54565be")
+        xhr.send();
+
+        xhr.onload = () => {
+            var json = JSON.parse (xhr.responseText);
+            this.datas = json;
+            
+
+        }
+
+
+    },
+});
